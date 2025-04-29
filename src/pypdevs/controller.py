e30fd5de47300e99a60d3d31b88bf25f85558808
Likhith Kanigolla <likhithkanigolla@gmail.com>
2025-04-30 03:32:47 +0530

    Removed Python Reserved Keywords

--- src/pypdevs/controller.py
+++ src/pypdevs/controller.py
@@ -548,8 +548,8 @@ class Controller(BaseSimulator):
         self.realtime = True
         self.threading_backend = ThreadingBackend(subsystem, args)
         self.rt_zerotime = time.time()
-        async = AsynchronousComboGenerator(generator_file, self.threading_backend)
-        self.asynchronous_generator = async
+        async_new = AsynchronousComboGenerator(generator_file, self.threading_backend)
+        self.asynchronous_generator = async_new
         self.realtime_starttime = time.time()
         self.portmap = ports
         self.model.listeners = listeners
